a06e938d03f8111aab60fa908f6b97ebbf594eeb
bsstibich <bsstibich@gmail.com>
2021-08-30 11:13:30 -0500

    start work on cypress code

--- cypress/integration/civicSearch.feature
+++ cypress/integration/civicSearch.feature
@@ -1,1 +1,5 @@
 Feature: civcSearcher
+    I want to search Facebook Marketplace for good sixth gen Honda Civics
+
+    Scenario: Go to marketplace and apply filters
+        Given I am in the Honda Civic marketplace page
--- cypress/integration/civicSearch/steps.spec.js
+++ cypress/integration/civicSearch/steps.spec.js
@@ -1,1 +1,11 @@
+const { cyan } = require("ansi-colors");
+
 //Search facebook for good civics to buy
+Cypress.config("viewportHeight", 1200);
+Cypress.config("viewportWidth", 1200);
+
+//========================================================================
+//Scenario: Go to marketplace and apply filters
+Given('I am in the Honda Civic marketplace page', () => {
+    cy.visit("https://www.facebook.com/marketplace/category/honda-civic");
+})
